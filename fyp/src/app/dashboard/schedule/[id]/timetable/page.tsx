1f567d70cbfb0209e0f57a46396be8623b210b1e
Bunlong Prak (Linux) <100819214+Bunlong-Prak@users.noreply.github.com>
2025-05-31 15:04:56 +0700

    temporary remove swtich to majorView button

--- fyp/src/app/dashboard/schedule/[id]/timetable/page.tsx
+++ fyp/src/app/dashboard/schedule/[id]/timetable/page.tsx
@@ -1,25 +1,14 @@
 "use client";
-
-import { useState } from "react";
 import TimetableViewClassroom from "@/app/dashboard/schedule/[id]/timetable/timetable-classroom-view";
-import MajorView from "@/app/dashboard/schedule/[id]/timetable/timetable-major-view";
-import ViewToggleButton from "@/components/custom/view-toggle";
 
 export default function TimetablePage() {
-  const [currentView, setCurrentView] = useState<'classroom' | 'major'>('classroom');
-
-  return (
-    <div>
-      {/* Top controls */}
-      <div className="flex justify-between items-center mb-6">
-        <h2 className="text-2xl font-bold">Timetable</h2>
-        <ViewToggleButton 
-          currentView={currentView} 
-          onViewChange={setCurrentView} 
-        />
-      </div>
-
-      {currentView === 'classroom' ? <TimetableViewClassroom /> : <MajorView />}
-    </div>
-  );
+    return (
+        <div>
+            {/* Top controls */}
+            <div className="flex justify-between items-center mb-6">
+                <h2 className="text-2xl font-bold">Timetable</h2>
+            </div>
+            <TimetableViewClassroom />
+        </div>
+    );
 }
